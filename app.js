5198863f58741a1dec0fbcf6f4788c0acd9bc0d8
Scott Norris <snorris@snorris-a01.vmware.com>
2019-10-01 10:10:08 +1000

    added port and host variables

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const forecast = require('./utils/forecast')
 const app = express();
 
-const PORT = process.env.HTTP_PORT || 3002
+const PORT = process.env.WEATHER_FORECAST_LB_SERVICE_PORT || 3002
 
 //const latitude = process.env.LATITUDE
 //const longitude = process.env.LONGITUDE
